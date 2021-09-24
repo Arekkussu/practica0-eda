90f0664b432b74505505d732d2869365b8e61691
Arekkussu <91312367+Arekkussu@users.noreply.github.com>
2021-09-24 10:14:29 +0200

    Initial commit

---
+++ Practica1_CalafatSerranoAlejandro_GarciaBaenaJulianManuel/Vec3DE.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practica1_CalafatSerranoAlejandro_GarciaBaenaJulianManuel
+{
+    class Vec3DE : Vectores   
+    {
+        public override void ImprimirPorPantalla(double[] vector)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double[] LeerVector(double[] vector)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double[] SumarVectores(double[] vector1, double[] vector2)
+        {
+            Console.WriteLine("Hola");
+            return vector1;
+        }
+    }
+}
